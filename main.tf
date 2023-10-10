fb986d9798a462fec41002be1f02b0659ac316e8
sainath1362 <141892780+sainath1362@users.noreply.github.com>
2023-10-10 11:21:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAZ3LFUPBL5TNKPGOE"
-  secret_key = "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"
+   region     = "us-east-1"
+   access_key = "AKIAZ3LFUPBLYI3Y5K73"
+   secret_key = "aklmKruX/64OZl+GAvj420VHWhvkhH/3GXP5vuLN"
 }
+resource "aws_instance" "ec2_example" {
 
-resource "aws_instance" "mainserver" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+   ami           = "ami-053b0d53c279acc90"
+   instance_type =  "t2.micro"
+   count = 3
 
-  tags = {
-    Name = "HelloWorld"
-  }
+   tags = {
+           Name = "Terraform EC2"
+   }
+
 }
